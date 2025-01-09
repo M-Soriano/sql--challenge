e817fcbbf2685bddd6137f74d13e82c04f7ced82
M-Soriano <104050536+M-Soriano@users.noreply.github.com>
2025-01-09 02:46:20 -0800

    In data_engineering.sql added title table schema

--- data/data_engineering.sql
+++ data/data_engineering.sql
@@ -4,6 +4,9 @@ CREATE TABLE departments(
     dept_name VARCHAR(50) NOT NULL
 );
 
---Creating dept_emp Table
-
+-- Creating titles table
+CREATE TABLE titles (
+    title_id VARCHAR(10) PRIMARY KEY,
+    title VARCHAR(50) NOT NULL
+);
 
