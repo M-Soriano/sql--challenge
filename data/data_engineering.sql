8cf951bdad869e624abab98b608e386b4662409f
M-Soriano <104050536+M-Soriano@users.noreply.github.com>
2025-01-09 02:56:39 -0800

    In data_engineering.sql added employees table schema

--- data/data_engineering.sql
+++ data/data_engineering.sql
@@ -10,3 +10,14 @@ CREATE TABLE titles (
     title VARCHAR(50) NOT NULL
 );
 
+-- Creating  employees table
+CREATE TABLE employees(
+    emp_no INTEGER(10) PRIMARY KEY,
+    emp_title_id VARCHAR(10) NOT NULL,
+    birth_date DATE NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    sex CHAR(1) NOT NULL,
+    hire_date DATE NOT NULL
+    FOREIGN KEY (emp_title_id) REFERENCES title(title_id)
+);
