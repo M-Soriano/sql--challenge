c2e286467b00fe50bb74e4a92062cbf75ba80c85
M-Soriano <104050536+M-Soriano@users.noreply.github.com>
2025-01-09 17:27:30 -0800

    In data_analysis.sql added query 3 and 4 requirements

--- data_Analysis.sql
+++ data_Analysis.sql
@@ -5,3 +5,13 @@ JOIN salaries s on e.emp_no=s.emp_no;
 SELECT  first_name, last_name, hire_date
 FROM employees 
 WHERE EXTRACT(YEAR FROM hire_date) = 1986;
+
+SELECT dm.dept_no, dt.dept_name, dm.emp_no, e.last_name, e.first_name
+FROM  dept_manager dm
+JOIN departments dt on dm.dept_no = dt.dept_no
+JOIN employees e on dm.emp_no = e.emp_no;
+
+SELECT de.dept_no, e.emp_no, e.last_name, e.first_name, (SELECT dept_name FROM departments dt WHERE dt.dept_no=de.dept_no) as dept_name
+FROM  employees e
+JOIN dept_emp de on e.emp_no = de.emp_no;--- could also be inner join
+
